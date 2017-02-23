8e3a6f4faba3da61f6a31567cf972c8459d8ac92
Ryan <ryan.genovese.11@gmail.com>
2017-02-23 22:09:03 +0000

    created test for static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  resources :pages
+ 
+  get 'static_pages/home'
+  
+  #resources :static_pages
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
